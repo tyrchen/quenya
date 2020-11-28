c0e79339f5a67639aaf639065f1e07b351df580a
Tyr Chen <tyr.chen@gmail.com>
2020-11-28 12:18:14 -0800

    support custom validator

--- config/config.exs
+++ config/config.exs
@@ -4,3 +4,7 @@ config :quenya,
   use_fake_handler: true,
   use_response_validator: true,
   apis: %{}
+
+config :ex_json_schema,
+       :custom_format_validator,
+       {QuenyaUtil.FormatValidator, :validate}
