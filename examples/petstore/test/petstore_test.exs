cee00cbf9d15688df6554de8bd7c207979574fa0
Tyr Chen <tyr.chen@gmail.com>
2020-11-29 11:12:04 -0800

    remove examples

--- examples/petstore/test/petstore_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PetstoreTest do
-  use ExUnit.Case
-  doctest Petstore
-
-  test "greets the world" do
-    assert Petstore.hello() == :world
-  end
-end
--- examples/todo/test/todo_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TodoTest do
-  use ExUnit.Case
-  doctest Todo
-
-  test "greets the world" do
-    assert Todo.hello() == :world
-  end
-end
