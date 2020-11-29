de96a994c8d06559e67f9a4ab089fc28e1ec3d15
Tyr Chen <tyr.chen@gmail.com>
2020-11-29 12:48:10 -0800

    fix typo

--- builder/mix.exs
+++ builder/mix.exs
@@ -5,7 +5,7 @@ defmodule QuenyaBuilder.MixProject do
   def project do
     [
       app: :quenya_builder,
-      version: @version
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
